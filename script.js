90bf778964ed5d36c57d49cf96aa85cadd4cd29c
Travis Mendenhall <twmwayne@gmail.com>
2020-09-29 15:37:23 -0600

    added function to check for periods, or not whole numbers

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+function isNumeric(num){
+  if (num.indexOf(".") > -1)
+  {
+    return false;
+  }
+
+  return !isNaN(num);
+
+}
+
 function generatePassword() {
   var lengthstr = window.prompt ("How mandy characters do you want in your password?:", "");
+  if (lengthstr == "")
+  {
+    alert("Error! Input does not meet criteria.");
+    return "";
+  }
+  
+  if (isNumeric(lengthstr) == false)
+  {
+    alert("Error! Not a number.");
+  }
+
 }
 // Write password to the #password input
 function writePassword() {
